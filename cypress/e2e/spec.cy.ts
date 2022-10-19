bc7f797165aac48acd2169e4c7ec233c359d77fe
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-19 16:27:06 -0500

    fix config cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,12 +1,6 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
-  });
-});
-
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('http://localhost:8080/');
+    cy.visit('');
     cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App');
   });
 });
