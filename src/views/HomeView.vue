0fbd9cb91525d98430d5ab837e7c1533f898e43c
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-19 10:07:44 -0500

    logic dropdown status

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <hero-top></hero-top>
     <div class="flex justify-center mt-8">
       <input-search v-model="search" @inputSearch="loadApi" />
-      <select-status v-model="status" v-if="collectionCharacters.length"  />
+      <select-status v-model="status" v-if="characters.length && isVisible"  />
     </div>
     <div class="container mx-auto flex flex-wrap justify-evenly py-7">
       <list-card :items="collectionCharacters"></list-card>
@@ -12,7 +12,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import axios from 'axios';
 
 import HeroTop from '@/components/home/hero-top.vue';
@@ -25,18 +25,31 @@ const search = ref('');
 const status = ref('all');
 const characters = ref<ResultReq[]>([]);
 const collectionCharacters = ref<ResultReq[]>([]);
+const isVisible = ref(false);
 
 const loadApi = async (val: string) => {
   search.value = '';
+  isVisible.value = false;
+  collectionCharacters.value = [];
   try {
     const res = await axios.get(`https://rickandmortyapi.com/api/character/?name=${val}`);
-    console.log(res.data);
+    // console.log(res.data);
     characters.value = res.data.results;
     collectionCharacters.value = characters.value;
-    // console.log(collectionCharacters.value);
+    isVisible.value = true;
   } catch (err: any) {
+    isVisible.value = false;
     console.log(err);
   }
 };
+
+watch(status, (val) => {
+  console.log('status', val);
+  collectionCharacters.value = characters.value.filter(x => {
+    // console.log(val)
+    if (val === 'All') return characters.value;
+    return x.status === val;
+  });
+});
 
 </script>
